e464bea0b33ea204a0d3d6b286d1adb219dbcccd
Aidar <aidarkhalilov4021@mail.ru>
2023-04-16 23:43:06 +0300

    Deleted documentation

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.lilkhalil.listenloud"},{"l":"com.lilkhalil.listenloud.config"},{"l":"com.lilkhalil.listenloud.controllers"},{"l":"com.lilkhalil.listenloud.model"},{"l":"com.lilkhalil.listenloud.repository"},{"l":"com.lilkhalil.listenloud.service"}];updateSearchResults();
